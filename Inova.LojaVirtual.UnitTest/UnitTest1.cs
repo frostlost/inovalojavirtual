37545d6a5ac4abb4d16c9c0612e9dacd224c8237
frostlost <frostlost@gmail.com>
2014-05-29 00:38:12 -0300

    initial commit

---
+++ Inova.LojaVirtual.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Inova.LojaVirtual.UnitTest
+{
+    [TestClass]
+    public class UnitTestInova
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
