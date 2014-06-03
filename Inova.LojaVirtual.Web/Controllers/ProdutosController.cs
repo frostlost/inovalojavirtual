5b67c71eb4405b1fa901abc0ebb8acd8fdd6ff1c
frostlost <frostlost@gmail.com>
2014-06-03 00:44:47 -0300

    Entity

---
+++ Inova.LojaVirtual.Web/Controllers/ProdutosController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using Inova.LojaVirtual.Dominio.Repositorio;
+
+namespace Inova.LojaVirtual.Web.Controllers
+{
+    public class ProdutosController : Controller
+    {
+
+        private ProdutosRepositorio _repositorio;
+        
+        //
+        // GET: /Produtos/
+        public ActionResult Index()
+        {
+            _repositorio = new ProdutosRepositorio();
+            var produtos = _repositorio.Produtos.Take(10);
+            return View(produtos);
+        }
+	}
+}
